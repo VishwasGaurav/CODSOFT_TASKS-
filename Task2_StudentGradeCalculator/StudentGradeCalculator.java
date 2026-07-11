d605c238dd3c64ecdf7a622c5f0ad5eba135852c
Vishwas Gaurav <vishwashsinghf100@gmail.com>
2026-07-12 00:16:15 +0530

    Update StudentGradeCalculator.java

--- Task2_StudentGradeCalculator/StudentGradeCalculator.java
+++ Task2_StudentGradeCalculator/StudentGradeCalculator.java
@@ -1,1 +1,66 @@
+import java.util.Scanner;
 
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("====================================");
+        System.out.println("   STUDENT GRADE CALCULATOR");
+        System.out.println("====================================");
+
+        System.out.print("Enter Number of Subjects: ");
+        int subjects = sc.nextInt();
+
+        int totalMarks = 0;
+
+        for (int i = 1; i <= subjects; i++) {
+
+            int marks;
+
+            while (true) {
+
+                System.out.print("Enter Marks for Subject " + i + " (0-100): ");
+                marks = sc.nextInt();
+
+                if (marks >= 0 && marks <= 100) {
+                    break;
+                } else {
+                    System.out.println("Invalid Marks! Please enter between 0 and 100.");
+                }
+            }
+
+            totalMarks += marks;
+        }
+
+        double average = (double) totalMarks / subjects;
+
+        String grade;
+
+        if (average >= 90)
+            grade = "A+";
+        else if (average >= 80)
+            grade = "A";
+        else if (average >= 70)
+            grade = "B";
+        else if (average >= 60)
+            grade = "C";
+        else if (average >= 50)
+            grade = "D";
+        else
+            grade = "F";
+
+        System.out.println("\n========== RESULT ==========");
+        System.out.println("Total Marks      : " + totalMarks);
+        System.out.printf("Average %%        : %.2f\n", average);
+        System.out.println("Grade            : " + grade);
+
+        if (grade.equals("F"))
+            System.out.println("Result           : FAIL");
+        else
+            System.out.println("Result           : PASS");
+
+        sc.close();
+    }
+}
