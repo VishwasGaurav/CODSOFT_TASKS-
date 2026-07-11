b2790edfcded22b366eeb9aa2cc420d0c151ae69
Vishwas Gaurav <vishwashsinghf100@gmail.com>
2026-07-12 00:27:14 +0530

    Update BankAccount.java

--- Task3_ATMInterface/BankAccount.java
+++ Task3_ATMInterface/BankAccount.java
@@ -1,1 +1,39 @@
+public class BankAccount {
 
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean withdraw(double amount) {
+
+        if (amount <= 0) {
+            System.out.println("Invalid amount.");
+            return false;
+        }
+
+        if (amount > balance) {
+            System.out.println("Insufficient Balance.");
+            return false;
+        }
+
+        balance -= amount;
+        return true;
+    }
+
+    public boolean deposit(double amount) {
+
+        if (amount <= 0) {
+            System.out.println("Invalid amount.");
+            return false;
+        }
+
+        balance += amount;
+        return true;
+    }
+}
